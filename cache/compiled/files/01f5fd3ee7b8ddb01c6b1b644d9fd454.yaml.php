1c1fbc754069f9ce83641c030fa9825ed4b64005
lokeshh <lokeshhsharma@gmail.com>
2016-09-19 18:17:58 +0000

    update .gitignore file

--- cache/compiled/files/01f5fd3ee7b8ddb01c6b1b644d9fd454.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/ubuntu/workspace/user/config/media.yaml',
-    'modified' => 1472214093,
-    'data' => [
-        
-    ]
-];
